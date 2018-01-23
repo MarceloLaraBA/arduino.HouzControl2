40d2ad688283b5bf9da621e57fd75a98525b691b
DarkAngel <darkangelba@gmail.com>
2018-01-23 19:55:20 -0300

    updated version

--- external_sensors/external_sensors.ino
+++ external_sensors/external_sensors.ino
@@ -10,12 +10,11 @@
 
 
 //Houz/Radio Setup
-#define rfRecvLed 9 //Led
+#define rfRecvLed 4 //Led
 #define rfCE 9      //RF pin 3 (CE)
 #define rfCS 10      //RF pin 4 (CS)
 RF24 radio(rfCE, rfCS);
 HouzDevices houz(external_node, radio, rfRecvLed, Serial);
-
 
 //light sensor
 #define lightSensorPin A0
@@ -35,32 +34,70 @@ void setup() {
 	//temperature
 	dallasTemp.begin();
 
-	Serial.println("\r\n-- runtime --");
+	Serial.println("\r\n-- external_node --");
+	Serial.println("r\t radio status");
+	Serial.println("l\t send light level");
+	Serial.println("t\t send temp level");
+	Serial.println("q\t query sensor values");
 }
 
+char serialIn;
 void loop() {
 	if (houz.radioRead()) {
 		handleCommand(houz.receivedData());
 	};
+	if (Serial.available() > 0) {
+		serialIn = Serial.read();
+		switch (serialIn)
+		{
+		case 'r':case 'R':
+			houz.radioReady();
+			break;
+
+		case 'l':case 'L':
+			houz.radioWrite(houz.encode(CMD_VALUE, external_lightSensor, lightLevel()), server_node);
+			break;
+
+		case 't':case 'T':
+			houz.radioWrite(houz.encode(CMD_VALUE, external_tempSensor, temperature()), server_node);
+			break;
+
+		case 'q':case 'Q':
+			Serial.println();
+			Serial.print("temp: ");
+			Serial.println(temperature());
+			Serial.print("light: ");
+			Serial.println(lightLevel());
+			break;
+
+		default:
+			Serial.print(serialIn);
+			Serial.println("?");
+			break;
+		}
+
+		Serial.println(">");
+
+	}
 }
 
 void handleCommand(deviceData device) {
-	Serial.println("handleCommand");
-	switch (device.id){
+	Serial.println("handleCommand: " + houz.deviceToString(device));
 	
-	case external_lightSensor:
-		delay(100);
-		houz.radioSend(CMD_VALUE, external_lightSensor, lightLevel());
-		break;
+	switch (device.id){
+		case external_lightSensor:
+			delay(500);
+			houz.radioSend(CMD_VALUE, external_lightSensor, lightLevel());
+			break;
 
-	case external_tempSensor:
-		delay(100);
-		houz.radioSend(CMD_VALUE, external_tempSensor, temperature()*100);
-		break;
+		case external_tempSensor:
+			delay(500);
+			houz.radioSend(CMD_VALUE, external_tempSensor, temperature()*100);
+			break;
 
-	default:
-		Serial.println("unhandled: " + houz.deviceToString(device));
-		break;
+		default:
+			Serial.println("unhandled: " + houz.deviceToString(device));
+			break;
 	}
 
 };
--- master_logger/master_logger.ino
+++ master_logger/master_logger.ino
@@ -1,6 +1,7 @@
 /*
  Name:		master_logger.ino
- Created:	05-Apr-17 21:40:50
+ Created:	05-Apr-17
+ Updated:	23-Jan-18
  Author:	DarkAngel
 */
 
@@ -8,30 +9,37 @@
 #include <HouzDevices.h>
 
 //radio setup
-#define rfRecvLed 47 //Led
-#define rfCE 49      //RF pin 3 (CE)
-#define rfCS 48      //RF pin 4 (CS)
+//#define rfRecvLed 47 //Led
+//#define rfCE 49      //RF pin 3 (CE)
+//#define rfCS 48      //RF pin 4 (CS)
+
+//wiring: D7>CS | D8>CE | (sck)D13>5 | (mosi)D11>6 | (miso)D12>7
+#define rfRecvLed 3 //RF online Led
+#define rfCE 9      //RF pin 3 (CE)
+#define rfCS 10     //RF pin 4 (CS)
 
 RF24 radio(rfCE, rfCS);
 HouzDevices houz(server_node, radio, rfRecvLed, Serial);
+deviceData device;
 
 void setup() {
-	//serial debug
 	Serial.begin(115200);
-	Serial.println("-- setup --");
 	houz.radioSetup();
-
-	Serial.println("");
-	Serial.println("\r\n-- runtime --");
+	Serial.println("\r\n-- master_logger --\r\n");
 }
 
-// the loop function runs over and over again until power down or reset
+// main loop ////////////////////////////////////////////////////////////////////////////////
 void loop() {
+	
+	// RF commands
 	if (houz.radioRead()) {
-		handleDevice(houz.receivedData());
-	}
+		handlePacket(action_rfReceived, houz.receivedData());
+	};
+
+  houz.serialRead();
 }
 
-void handleDevice(deviceData device) {
-	Serial.println(houz.deviceToString(device));
-}
+// packet handling ///////////////////////////////////////////////////////////////////////////
+void handlePacket(u8 action, deviceData device) {
+	Serial.println(houz.packetToString(action, device));
+};
